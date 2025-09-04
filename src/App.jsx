5151f5a48f2fb73496a326863788cc99d2fdba62
heisenberg1122 <bern.github@gmail.com>
2025-09-04 14:11:11 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <header><h1>Header</h1></header>
+        <div className="container1">
+          <aside className="container2">
+            <article className="hero"><h1>Hero</h1></article>
+            <article className="sideBar"><h1>Sidebar</h1></article>
+          </aside>
+          <div className="container3">
+            <main className="mainContent"><h1>Main Content</h1></main>
+            <article className="extraContent"><h1>Extra Content</h1></article>
+          </div>
+        </div>
+        <div className="container4">
+          <section className="relIm"><h1>Related Images</h1></section>
+          <section className="relPos"><h1>Related Posts</h1></section>
+        </div>
+        <footer><h1>Footer</h1></footer>
+      </div>
+    </>
+  )
+}
+
+export default App
